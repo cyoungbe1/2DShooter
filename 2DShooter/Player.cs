32d16e0f7af6570f9ee776df8ab2bc950f03686d
Christopher Youngbeck <cyoungbe1@gmail.com>
2022-09-05 21:16:37 -0400

    first commit

---
+++ 2DShooter/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DShooter
+{
+    public class Player
+    {
+        public char weapon1;
+        public char weapon2;
+        public char weapon3;
+        public char item1;
+        public char item2;
+        public int totalPoints;
+        public string name;
+
+        public Player() { }
+
+        public override string ToString()
+        {
+            return String.Format("{weapon1,weapon2,weapon3,item1,item2,totalPoints");
+        }
+    }
+}
---
+++ 2DShooter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DShooter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            player1.weapon1 = 'a';
+            player1.weapon2 = 'b';
+            player1.weapon3 = 'c';
+            player1.item1 = 'd';
+            player1.item2 = 'e';
+            player1.totalPoints = 0;
+
+            player1.name = "badassbob";
+
+            Console.Clear();
+            Console.WriteLine("name " + player1.name, "Total Points" + player1.totalPoints, "Item 1" + player1.item1, "Item 2" + player1.item2, "Weapon 1" + player1.weapon1, "Weapon2" + player1.weapon2, "Weapon 3" + player1.weapon3);
+        }
+    }
+}
